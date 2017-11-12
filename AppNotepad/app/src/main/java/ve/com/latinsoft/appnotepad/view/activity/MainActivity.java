0b538d223e29cbace3538a12ad6c119875a7f5fe
Manuel28G <man281192@gmail.com>
2017-11-12 04:06:45 -0400

    Activity Principal

---
+++ AppNotepad/app/src/main/java/ve/com/latinsoft/appnotepad/view/activity/MainActivity.java
@@ -0,0 +1,48 @@
+package ve.com.latinsoft.appnotepad.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import ve.com.latinsoft.appnotepad.R;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    private static boolean press=false;
+    Toolbar toolbar;
+    NavigationView naviationView;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        naviationView = (NavigationView) findViewById(R.id.navigationView);
+        naviationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.isChecked()){
+                    item.setChecked(false);
+                }
+                else
+                {
+                    item.setChecked(true);
+                }
+                return false;
+            }
+        });
+
+    }
+}
